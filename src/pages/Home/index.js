c0c94ca9cb4c3355547a5167402d7ae07a153ad4
joaoptgaino <joaoptgaino@hotmail.com>
2020-07-01 23:18:45 -0300

    Home front-end and started to work on the weather api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import {FaSearchLocation,FaSearch} from 'react-icons/fa';
+import { Link,useHistory} from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { FaSearchLocation, FaSearch } from 'react-icons/fa';
 import axios from 'axios';
+import './styles.css';
 /* import {useGeolocation} from 'react-use'; */
 
 const Home = () => {
@@ -11,15 +13,16 @@ const Home = () => {
     const [cityName, setCityName] = useState();
     /* const [userIp, setUserIp] = useState(); */
     const API_KEY = 'b9a9fdac9468e9d249cb59ed9e17a0a5';
-    const IP = '191.37.40.229'
-
-    useEffect(()=>{
+    const IP = '191.37.40.229';
+    const { handleSubmit, errors,register } = useForm();
+    const history = useHistory();
+    useEffect(() => {
         axios.get('https://extreme-ip-lookup.com/json')
-        .then(response=>{
-            setCityName(response.data.city);
-            console.log(cityName);
-        })
-    },[])
+            .then(response => {
+                setCityName(response.data.city);
+                console.log(cityName);
+            })
+    }, [])
     /* useEffect(() => {
         axios.get(`http://api.ipstack.com/${IP}?access_key=${API_KEY}`)
             .then(response => {
@@ -29,12 +32,21 @@ const Home = () => {
         setLatitude(state.latitude);
         setLongitude(state.longitude);
     },[]) */
+    const onSubmit = data => {
+        console.log(data);
+        history.goForward('/weather')
+    }
+
     return (
         <div className="page-content">
             <div className="text-content">
                 <h1>Hello! Please write your city name</h1>
-                <input type="text" value={cityName}/>
-                <button type="submit"><FaSearch/></button>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <input type="text" name="city" placeholder={`Ex: ${cityName}`} ref={register({required:true})} className="searchForm" />
+                        <button type="submit" className="btn mb-2"><FaSearch />Search</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
--- src/pages/Weather/index.js
+++ src/pages/Weather/index.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import axios from 'axios';
 
 const Weather = () => {
     const LOCATION = '5128581';
     const APIKEY = '0b177100caf71a656b93bfd11c06f133';
+    const [temperature,setTemperature] = useState();
+    const [city,setCity] = useState();
+    useEffect(()=>{
+        axios.get('https://extreme-ip-lookup.com/json')
+        .then(response=>{
+            setCity(response.data.city);
+            console.log(city);
+        })
+    },[])
     useEffect(() => {
-        api.get(`?id=${LOCATION}&appid=${APIKEY}`)
+        api.get(`?q=Vancouver&appid=${APIKEY}`)
             .then(response => {
                 console.log(response.data);
+                let temp = response.data.main.temp;
+                const tempCelsius = temp-273.15;
+                setTemperature(tempCelsius.toFixed(1));
             })
-    })
+    },[]);
+    
     return (
         <div>
             <h1>Weather</h1>
+            <h1>{temperature}ºC</h1>
             <Link to="/">Home</Link>
         </div>
     );
